d1a3dae3cb0232d408c8b1440b28e277f01bc57d
hmiao <739025250@qq.com>
2021-08-06 18:24:25 +0800

    [feature] package

---
+++ setup.py
@@ -0,0 +1,65 @@
+# type: ignore
+# -*- coding: utf-8 -*-
+import shlex
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+        self.pytest_args = ["tests"] + shlex.split(self.pytest_args)
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name="delta_node",
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    exclude_package_data={"": [".gitignore"]},
+    install_requires=[
+        'grpcio~=1.39.0',
+        'cryptography~=3.4.7',
+        'pandas~=1.2.3',
+        'SQLAlchemy~=1.4.21',
+        'tenacity~=8.0.1',
+        'uvicorn~=0.14.0',
+        'requests~=2.25.1',
+        'numpy~=1.20.1',
+        'pydantic~=1.8.2',
+        'fastapi~=0.68.0',
+        'Pillow~=8.3.1',
+        'protobuf~=3.17.3',
+        'PyYAML~=5.4.1',
+        'python-multipart~=0.0.5',
+        'grpcio==1.39.0'
+    ],
+    tests_require=["pytest"],
+    cmdclass={"test": PyTest},
+    test_suite="tests",
+    entry_points={
+        'console_scripts': ['delte_node_start=delta_node.run:run']
+    },
+    zip_safe=False,
+    author="miaohong",
+    author_email="73902525@qq.com",
+    description="delta node",
+    python_requires=">=3.6",
+)
