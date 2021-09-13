073760f42ab8e170dbbc8258bd33c45f83c05067
hmiao <739025250@qq.com>
2021-09-13 11:54:46 +0800

    [feature] rm test

--- secret_share_test.py
+++
@@ -1,9 +0,0 @@
-from delta_node.crypto import shamir
-import random
-
-if __name__ == "__main__":
-    seed = random.randint(1, shamir.PRIME - 1)
-    ss = shamir.SecretShare(2)
-    shares = ss.make_shares(seed, 2)
-    res = ss.resolve_shares(shares)
-    assert seed == res
