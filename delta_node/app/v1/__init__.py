645076b62bacb0edfd4842b95b90bfbd1d9f3afa
hmiao <739025250@qq.com>
2022-01-13 20:23:01 +0800

    add nodes api

--- delta_node/app/v1/__init__.py
+++ delta_node/app/v1/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from . import task, coord
+from . import coord, node, task
 
 __all__ = ["router"]
 
 router = APIRouter()
 router.include_router(task.router)
 router.include_router(coord.router)
+router.include_router(node.router)
