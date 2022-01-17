af495f957232f2edad45368818e56246e0ad41bd
hmiao <739025250@qq.com>
2022-01-17 17:59:08 +0800

    add tx hash to log

--- delta_node/log/file_handler.py
+++ delta_node/log/file_handler.py
@@ -1,23 +1,52 @@
-import os
 import logging
+import os
+from logging import LogRecord
 from logging.handlers import RotatingFileHandler
+from typing import List
 
 from .. import config
 
-if not os.path.exists(config.log_dir):
-    os.makedirs(config.log_dir, exist_ok=True)
 
-fmt = '%(asctime)s.%(msecs)03d - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s'
-datefmt = '%Y-%m-%d %H:%M:%S'
+def create_handlers() -> List[logging.Handler]:
+    datefmt = "%Y-%m-%d %H:%M:%S"
+    common_fmt = "%(asctime)s.%(msecs)03d - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s"
+    tx_fmt = common_fmt + " tx hash: %(tx_hash)s"
 
-formatter = logging.Formatter(fmt, datefmt)
+    common_formatter = logging.Formatter(common_fmt, datefmt)
+    tx_formatter = logging.Formatter(tx_fmt, datefmt)
 
-log_filename = os.path.join(config.log_dir, "log.txt")
-handler = RotatingFileHandler(
-    log_filename,
-    encoding="utf-8",
-    delay=True,
-    maxBytes=50 * 1024 * 1024,
-    backupCount=5,
-)
-handler.setFormatter(formatter)
+    class CommonFilter(logging.Filter):
+        def filter(self, record: LogRecord) -> bool:
+            tx_hash = getattr(record, "tx_hash", None)
+            return tx_hash is None
+
+    class TxFilter(logging.Filter):
+        def filter(self, record: LogRecord) -> bool:
+            tx_hash = getattr(record, "tx_hash", None)
+            return tx_hash is not None
+
+    if not os.path.exists(config.log_dir):
+        os.makedirs(config.log_dir, exist_ok=True)
+    log_filename = os.path.join(config.log_dir, "log.txt")
+
+    common_handler = RotatingFileHandler(
+        log_filename,
+        encoding="utf-8",
+        delay=True,
+        maxBytes=50 * 1024 * 1024,
+        backupCount=5,
+    )
+    common_handler.setFormatter(common_formatter)
+    common_handler.addFilter(CommonFilter())
+
+    tx_handler = RotatingFileHandler(
+        log_filename,
+        encoding="utf-8",
+        delay=True,
+        maxBytes=50 * 1024 * 1024,
+        backupCount=5,
+    )
+    tx_handler.setFormatter(tx_formatter)
+    tx_handler.addFilter(TxFilter())
+
+    return [common_handler, tx_handler]
