2b951bfec39585f96ccd520ff77764a900e93701
hmiao <739025250@qq.com>
2022-09-26 21:15:47 +0800

    refactor registry

--- delta_node/registry/registry.py
+++ delta_node/registry/registry.py
@@ -5,8 +5,9 @@ from typing import Optional
 import sqlalchemy as sa
 from sqlalchemy.exc import NoResultFound
 from async_lru import alru_cache
-from delta_node import chain, config, db, entity
-from delta_node.entity.node import Node
+from delta_node import config, db
+from delta_node.entity.identity import Node
+from delta_node.chain import identity
 
 __all__ = ["register", "get_node_address", "unregister"]
 
@@ -17,9 +18,9 @@ _logger = logging.getLogger(__name__)
 @alru_cache
 async def get_node_address() -> str:
     async with db.session_scope() as sess:
-        q = sa.select(entity.Node).where(entity.Node.id == 1)
+        q = sa.select(Node).where(Node.id == 1)
         try:
-            node: entity.Node = (await sess.execute(q)).scalars().one()
+            node: Node = (await sess.execute(q)).scalars().one()
             return node.address
         except NoResultFound:
             _logger.error("node has not been registered")
@@ -31,19 +32,19 @@ async def register(
     name: str = config.node_name,
 ):
     async with db.session_scope() as sess:
-        q = sa.select(entity.Node).where(entity.Node.id == 1)
-        node: Optional[entity.Node] = (await sess.execute(q)).scalars().one_or_none()
+        q = sa.select(Node).where(Node.id == 1)
+        node: Optional[Node] = (await sess.execute(q)).scalars().one_or_none()
 
         if node:
             # join first to avoid address changed when connect to monkey chain connector
-            await chain.get_client().join(url, name)
+            await identity.get_client().join(url, name)
             updated = False
             if node.url != url:
-                await chain.get_client().updaet_url(node.address, url)
+                await identity.get_client().updaet_url(node.address, url)
                 node.url = url
                 updated = True
             if node.name != name:
-                await chain.get_client().update_name(node.address, name)
+                await identity.get_client().update_name(node.address, name)
                 node.name = name
                 updated = True
             if updated:
@@ -52,8 +53,8 @@ async def register(
             _logger.info(f"registered node, node address: {node.address}")
 
         else:
-            _, address = await chain.get_client().join(url, name)
-            node = entity.Node(url=url, name=name, address=address)
+            _, address = await identity.get_client().join(url, name)
+            node = Node(url=url, name=name, address=address)
             sess.add(node)
             await sess.commit()
             await sess.refresh(node)
@@ -62,10 +63,10 @@ async def register(
 
 async def unregister():
     address = await get_node_address()
-    await chain.get_client().leave(address)
+    await identity.get_client().leave(address)
 
     async with db.session_scope() as sess:
-        q = sa.select(entity.Node).where(entity.Node.id == 1)
+        q = sa.select(Node).where(Node.id == 1)
         node = (await sess.execute(q)).scalar_one()
 
         await sess.delete(node)
