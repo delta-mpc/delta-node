29059c6a9059d309f8a3740c7a9fc9e690839294
hmiao <739025250@qq.com>
2022-09-28 15:14:33 +0800

    build proto: add zk

--- build_proto.sh
+++ build_proto.sh
@@ -10,6 +10,7 @@ mkdir -p delta_node/chain/datahub
 mkdir -p delta_node/chain/hlr
 mkdir -p delta_node/chain/subscribe
 mkdir -p delta_node/chain/transaction
+mkdir -p delta_node/zh
 
 python3 -m grpc_tools.protoc -Idelta_node/proto --python_out=delta_node/chain/identity --grpclib_python_out=delta_node/chain/identity --mypy_out=delta_node/chain/identity delta_node/proto/identity.proto
 python3 -m grpc_tools.protoc -Idelta_node/proto --python_out=delta_node/chain/horizontal --grpclib_python_out=delta_node/chain/horizontal --mypy_out=delta_node/chain/horizontal delta_node/proto/horizontal.proto
@@ -17,3 +18,4 @@ python3 -m grpc_tools.protoc -Idelta_node/proto --python_out=delta_node/chain/da
 python3 -m grpc_tools.protoc -Idelta_node/proto --python_out=delta_node/chain/hlr --grpclib_python_out=delta_node/chain/hlr --mypy_out=delta_node/chain/hlr delta_node/proto/hlr.proto
 python3 -m grpc_tools.protoc -Idelta_node/proto --python_out=delta_node/chain/subscribe --grpclib_python_out=delta_node/chain/subscribe --mypy_out=delta_node/chain/subscribe delta_node/proto/subscribe.proto
 python3 -m grpc_tools.protoc -Idelta_node/proto --python_out=delta_node/chain/transaction --grpclib_python_out=delta_node/chain/transaction --mypy_out=delta_node/chain/transaction delta_node/proto/transaction.proto
+python3 -m grpc_tools.protoc -Idelta_node/proto --python_out=delta_node/zk --grpclib_python_out=delta_node/zk --mypy_out=delta_node/zk delta_node/proto/delta-zk.proto
