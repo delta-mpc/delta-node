1c3fa32776ef93dadc190b0ca10cd41d77844143
hmiao <739025250@qq.com>
2022-09-28 18:39:37 +0800

    complete mimc7 hash

--- delta_node/utils/__init__.py
+++ delta_node/utils/__init__.py
@@ -1,6 +1,16 @@
 from .arr import make_mask
 from .commitment import calc_commitment
 from .constant import data_block_size, q
+from .mimc7 import calc_weight_commitment, calc_data_commitment
 from .precision import fix_precision, unfix_precision
 
-__all__ = ["make_mask", "fix_precision", "unfix_precision", "calc_commitment", "q", "data_block_size"]
+__all__ = [
+    "make_mask",
+    "fix_precision",
+    "unfix_precision",
+    "calc_commitment",
+    "q",
+    "data_block_size",
+    "calc_weight_commitment",
+    "calc_data_commitment"
+]
