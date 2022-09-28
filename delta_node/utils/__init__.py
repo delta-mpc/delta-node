2cfd4b8a057245c5af5a74fe5547d719f6597335
hmiao <739025250@qq.com>
2022-09-28 15:13:22 +0800

    utils: constant

--- delta_node/utils/__init__.py
+++ delta_node/utils/__init__.py
@@ -1,3 +1,6 @@
 from .arr import make_mask
-from .precision import fix_precision, unfix_precision
 from .commitment import calc_commitment
+from .constant import data_block_size, q
+from .precision import fix_precision, unfix_precision
+
+__all__ = ["make_mask", "fix_precision", "unfix_precision", "calc_commitment", "q", "data_block_size"]
---
+++ delta_node/utils/constant.py
@@ -0,0 +1,9 @@
+
+__all__ = ["q", "data_block_size"]
+
+def q() -> int:
+    return 21888242871839275222246405745257275088548364400416034343698204186575808495617
+
+
+def data_block_size() -> int:
+    return 128
