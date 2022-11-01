fix type annotation error in utils.precision

delta_node/utils/precision.py
@@ -3,13 +3,13 @@ from delta.core.task import AggValueType
 
 
 def fix_precision(arr: AggValueType, precision: int) -> AggValueType:
-    arr = arr.astype(np.float64)
-    arr = arr * (10**precision)
-    arr = arr.astype(np.int64)
-    return arr
+    _arr = arr.astype(np.float64)
+    _arr = _arr * (10**precision)
+    _arr = _arr.astype(np.int64)
+    return _arr
 
 
 def unfix_precision(arr: AggValueType, precision: int) -> AggValueType:
-    arr = arr.astype(np.float64)
-    arr = arr / (10**precision)
-    return arr
+    _arr = arr.astype(np.float64)
+    _arr = _arr / (10**precision)
+    return _arr